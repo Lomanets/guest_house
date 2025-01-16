e876aaf5cb8048c8a12d6dac9da621536836898f
Lomanets <dimalomanec@gmail.com>
2025-01-16 02:35:14 +0200

    feat: add section rooms and slider

---
+++ assets/scripts/script.js
@@ -0,0 +1,29 @@
+const rooms = document.querySelectorAll('.room');
+const buttonSlider = document.querySelectorAll('.rooms-description__button-item')
+let countSlider = 0
+
+
+rooms.forEach((item) => {
+    item.classList.add('none');
+});
+
+rooms[0].classList.remove('none');
+
+
+buttonSlider.forEach((item) => {
+    item.addEventListener('click', (i) => {
+        if (item.classList.contains('buttom-disabled')) return;
+
+        rooms[countSlider].classList.add('none');
+
+        if (item.classList.contains('button-left')) {
+            countSlider -= 1;
+        } else {
+            countSlider += 1;
+        }
+
+        rooms[countSlider].classList.remove('none');
+
+        
+    });
+});
